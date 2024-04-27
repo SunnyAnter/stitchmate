2af48870a1ac6543dba8dd841372f6abd6288ddc
Sunny Anter <sunny@veskim.com>
2024-04-27 11:04:27 +0100

    feat: changed toggle sound and hook to tsx

--- src/components/ui/toggle-sound.tsx
+++ src/components/ui/toggle-sound.tsx
@@ -4,9 +4,9 @@ import {useStore} from '@/app/store';
 import {FaVolumeHigh, FaVolumeXmark} from "react-icons/fa6";
 import {Button} from './button';
 
-export default function ToggleSound () {
-  const {clickSoundEnabled, toggleSound} = useStore();
-  const toggleSoundIcon = clickSoundEnabled ? <FaVolumeHigh /> : <FaVolumeXmark />;
+export default function ToggleSound ():JSX.Element{
+  const { clickSoundEnabled,toggleSound } = useStore();
+  const toggleSoundIcon: JSX.Element = clickSoundEnabled ? <FaVolumeHigh /> : <FaVolumeXmark />;
 
   return (
     <Button size="icon" onClick={toggleSound} className='bg-sienna-400'>
