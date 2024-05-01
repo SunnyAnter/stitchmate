236c8dd0681a126aca3a4f2b26da8b608c164630
Sunny Anter <sunny@veskim.com>
2024-05-01 08:36:46 +0100

    feat: testttss babyy

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,17 +1,20 @@
-import { render,screen } from '@testing-library/react';
-// import Page from '../src/app/page';
+import { render,screen, fireEvent } from '@testing-library/react';
+import Counter from '../src/components/counter/counter';
 import '@testing-library/jest-dom';
-// import Sam from '../src/components/sam'
 
-describe('Main Page', () => {
-  // it('main page renders correctly', () => {
-  //   const { asFragment } = render(<Page/>);
-  //   expect(asFragment()).toMatchSnapshot();
-  // });
-  it('renders sam', async () => {
-    expect(true).toBe(true)
-    // render(<Sam />);
-    // await screen.findByRole('h1')
-    // expect(screen.getByRole('h1')).toHaveTextContent('hey')
-  })
-});
+describe('Counter', () => {  
+  render(<Counter />);
+ it('renders correctly', () => {
+    const blob = screen.querySelector('svg')
+    expect(blob).toBeInTheDocument();
+ });
+  
+  it('increments count when button is clicked', () => {
+    const button = screen.getByTestId('counter-button');
+    expect(button).toBeInTheDocument();
+    const count = parseInt(screen.getByTestId('count').textContent);
+    fireEvent.click(button);
+    const afterClickCount = parseInt(screen.getByTestId('count').textContent);
+    expect(afterClickCount).toBe(count+1);
+  });
+})
